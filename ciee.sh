66dcd5b1b1d94fce63c407011b062ce3baf33853
MR.51MB4H <48198896+kingcracker@users.noreply.github.com>
2019-04-11 20:12:01 +0700

    Add files via upload

---
+++ ciee.sh
@@ -0,0 +1,101 @@
+# ! /usr/bin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo  $green
+cat hay.txt
+echo " "
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : Mr.51MB4H"
+echo "\033[37;1mTeam   : Indenpendent Cyber Team"
+echo "\033[35;1mJenis Tool : buat para bucin!!"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Kata Kata Gombal LUCU"
+echo "[===============================================]>"
+echo $purple "2.> Kata Kata Gombal B.INGGRIS"
+echo "[===============================================]>"
+echo $cyan "3.>Kata Kata Gombalan Maut"
+echo "[===============================================]>"
+echo $red "4.> Kata Kata Gombal Buat Pacar Tersenyum"
+echo "[===============================================]>"
+echo $white "5.> Kata Kata Romantis dilan "
+echo "[===============================================]>"
+echo $yellow "0.> keluar"
+echo "[+]===============================================[+]"
+echo "\033[32;1m"
+read -p "fight@King Cracker"~¥ cie
+
+if [ $cie = 1 ] || [ $cie = 01 ]
+then
+clear
+figlet "BUCIN" | lolcat
+mv gokil.txt $HOME
+cd $HOME
+termux-setup-storage
+mv gokil.txt /sdcard
+echo $blue "file di memori cari gokil.txt"
+echo $green"hhahhh dasar bucin lu"
+fi
+
+if [$cie = 2 ] || [ $cie = 02 ]
+then
+clear
+toilet -f slant --gay "Bucin lu"
+mv inggris.txt $HOME
+cd $HOME
+termux-setup-storage
+mv ing.txt /sdcard
+echo $blue "file dimemori cari ing.txt"
+echo $green "fighting ya.."
+fi
+
+if [$cie = 3 ] || [ $cie = 03 ]
+then
+clear
+toilet -f slant --gay "CIEE"
+mv maut.txt $HOME
+cd $HOME
+termux-setup-storage
+mv maut.txt /sdcard
+echo $blue "file ada dimemori cari maut.txt"
+echo $yellow "thanks"
+fi
+
+if [$cie = 4 ]|| [ $cie = 04 ]
+then
+toilet -f standard --F "baper"
+mv senyum.txt $HOME
+cd $HOME
+termux-setup-storage
+mv senyum.txt /sdcard
+echo $blue "file ada dimemori cari senyum.txt"
+fi
+
+if [$cie = 5 ] || [ $cie = 05 ]
+then
+clear
+toilet -f slant --gay  "gombal"
+mv roman.txt $HOME
+cd $HOME
+termux-setup-storage
+mv roman.txt /sdcard
+echo $blue "file dimemori cafi roman.txt"
+fi
+
+if [$cie = 0 ]
+then
+clear
+echo $blue "terima kasih udah gunakan tools ini"
+echo $green "berjuanglah dan dapatkan dia"
+exit
+fi
